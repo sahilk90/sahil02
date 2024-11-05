072ce59e3d43b51fd8728e9fff8454050bc78afe
sahilk90 <sahilshah6829@outlook.com>
2024-11-05 10:43:16 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,37 @@
+#API_SAUKE
+#SASUKE_FOEMAT
+#BYSasUKE_API_KEY FAVDAUTCREHY
+#OWNER - @offx_sahil
+#KEY_REDEMPTION_1711
+#ACCESS_KEY_1711
+#FAILED_SasUKE_1711
+#HFAHYOYO_YTIATII_1575757_AS4575AA_57A7ASUYUT_WUUTIUU_4687_1711
+#JOIN - https://t.me/ddos_kasukabe
+#CHANNEL- https://t.me/ddos_kasukabe
+
+# Don't use quotes( " and ' )
+#SCRIPT BY SASUKE
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7276555883:AAECVqvWzsLJvWQHC-LgjDK-h_Y-l_b4UHQ")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@offx_sahil")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6512242172")
+
+#API_SAUKE
+#SASUKE_FOEMAT
+#BYSasUKE_API_KEY FAVDAUTCREHY
+#OWNER - @offx_sahil
+#KEY_REDEMPTION_1711
+#ACCESS_KEY_1711
+#FAILED_SasUKE_1711
+#HFAHYOYO_YTIATII_1575757_AS4575AA_57A7ASUYUT_WUUTIUU_4687_1711
+#JOIN - @KASUKABE DDOS
+#CHANNEL- https://t.me/ddos_kasukabe
+
+
+
+  
